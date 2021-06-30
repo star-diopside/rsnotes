eebc1c0daa0b47f49796f8b495932692e515c32d
se-yoshi <se.yoshi@gmail.com>
2021-06-30 20:01:27 +0900

    first commit

---
+++ src/test/java/jp/gr/java_conf/stardiopside/rsnotes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gr.java_conf.stardiopside.rsnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
