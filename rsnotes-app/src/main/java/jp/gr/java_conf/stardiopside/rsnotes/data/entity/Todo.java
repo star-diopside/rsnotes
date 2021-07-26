8eb89eac24c3c1d9e99046b779bd6210072da04d
se-yoshi <se.yoshi@gmail.com>
2021-07-27 02:07:15 +0900

    不要なカラムマッピング設定を削除

--- rsnotes-app/src/main/java/jp/gr/java_conf/stardiopside/rsnotes/data/entity/Todo.java
+++ rsnotes-app/src/main/java/jp/gr/java_conf/stardiopside/rsnotes/data/entity/Todo.java
@@ -3,7 +3,6 @@ package jp.gr.java_conf.stardiopside.rsnotes.data.entity;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.*;
-import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -22,19 +21,15 @@ public class Todo {
     @Length(max = 2048)
     private String text;
 
-    @Column("created_at")
     @CreatedDate
     private LocalDateTime createdAt;
 
-    @Column("created_by")
     @CreatedBy
     private String createdBy;
 
-    @Column("updated_at")
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
-    @Column("updated_by")
     @LastModifiedBy
     private String updatedBy;
 
--- rsnotes-app/src/main/java/jp/gr/java_conf/stardiopside/rsnotes/data/entity/User.java
+++ rsnotes-app/src/main/java/jp/gr/java_conf/stardiopside/rsnotes/data/entity/User.java
@@ -2,7 +2,6 @@ package jp.gr.java_conf.stardiopside.rsnotes.data.entity;
 
 import lombok.*;
 import org.springframework.data.annotation.*;
-import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -25,19 +24,15 @@ public class User {
 
     private boolean enabled;
 
-    @Column("created_at")
     @CreatedDate
     private LocalDateTime createdAt;
 
-    @Column("created_by")
     @CreatedBy
     private String createdBy;
 
-    @Column("updated_at")
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
-    @Column("updated_by")
     @LastModifiedBy
     private String updatedBy;
 
