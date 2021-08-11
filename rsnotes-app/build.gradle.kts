9b71420d93da93dc3375a500e21804524a556112
se-yoshi <se.yoshi@gmail.com>
2021-08-11 18:28:03 +0900

    データベース初期化の構成を変更

--- rsnotes-app/build.gradle.kts
+++ rsnotes-app/build.gradle.kts
@@ -4,11 +4,14 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     runtimeOnly("io.r2dbc:r2dbc-postgresql")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.liquibase:liquibase-core")
     runtimeOnly("org.thymeleaf.extras:thymeleaf-extras-java8time")
     runtimeOnly("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
     runtimeOnly("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,2 @@
 rootProject.name = "rsnotes"
 include("rsnotes-app")
-include("rsnotes-db-setup")
